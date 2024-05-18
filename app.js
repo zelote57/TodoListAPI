acf0681ad0289235bfe1fc38b52d9b68386d2a68
carlosD <carlos.dimitrakis@tecnologicoliceocristiano.edu.ec>
2024-05-17 20:52:42 -0500

    Added Control Error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const {v4: uuiv4} = require('uuid');
 
 const taskRoutes = require('./routes/task-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
