f55cd58e9e7dbc49e876d03fe0e0f1af19b37b9d
carlosD <carlos.dimitrakis@tecnologicoliceocristiano.edu.ec>
2024-05-17 21:15:06 -0500

    Added Unknown Error

--- app.js
+++ app.js
@@ -9,5 +9,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use('api/task', taskRoutes);
 
+app.use((req, res, next)=>{
+    const error = new HttpError('Ruta no existente', 404);
+    throw error;
+});
+
+app.use((error, req, res, next)=>{
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message : error.message || 'Error desconocido'});
+});
+
 
 app.listen(4000);
