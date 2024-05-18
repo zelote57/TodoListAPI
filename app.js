a06ca11c4f2cdacfe9cf0ba23920bc53adc99c27
Gustavo Lozada <zelote57@gmail.com>
2024-05-17 20:47:31 -0500

    delete uuid from js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {v4: uuiv4} = require('uuid');
 
 const taskRoutes = require('./routes/task-routes');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.listen(4000);
